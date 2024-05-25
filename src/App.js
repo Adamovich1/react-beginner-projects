06251f847777568004f6dedfb10268622e9e0b39
Adam <A.O.Adam006@gmail.com>
2024-05-25 16:49:21 +0300

    Добавил useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './index.scss';
-
+import React from 'react'
 function App() {
+  const [count, setCount] =  React.useState(0)
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
+        <h1>{count}</h1>
         <button className="minus">- Минус</button>
         <button className="plus">Плюс +</button>
       </div>
